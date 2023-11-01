6e47795e828c4c34bb71052bea72e60718d69a7e
SHRIYAKOUL <shriyakoul17@gmail.com>
2023-11-01 13:24:55 +0530

    Few changes in the code

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,17 @@
 // ./config/mongoose.js
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1/issue_tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+module.exports = mongoose;
+*/
+const mongoose = require('mongoose');
+
+// replace the "<password>" with your actual password
+const uri = "mongodb+srv://shriyakoul17:KoulShriya2001@issue-teacker-cluster.tixy1xg.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
